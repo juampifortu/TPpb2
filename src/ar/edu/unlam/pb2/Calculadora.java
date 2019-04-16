2b45fac790721968dd901ea98a0d084537868a1c
juampifortu <juampideamerica@gmail.com>
2019-04-16 12:49:02 -0300

    Segundo Commit

--- src/ar/edu/unlam/pb2/Calculadora.java
+++ src/ar/edu/unlam/pb2/Calculadora.java
@@ -6,27 +6,40 @@ public class Calculadora {
 	public static void main(String[] args) {
 		
 		teclado = new Scanner(System.in);
-		Integer a;
-		Integer b;
+		Integer a=0;
+		Integer b=0;
 		Integer opcion;
-		Integer resultado;
+		Integer resultado=0;
  do{
- System.out.println("Ingrese su operación \n 1 para sumar \n 2 para restar \n 3 para multiplicar \n 4 para dividir");
- opcion = teclado.nextInt();
+	 System.out.println("Ingrese su operación \n 1 para sumar \n 2 para restar \n 3 para multiplicar \n 4 para dividir");
+	 opcion = teclado.nextInt();
  }while(opcion < 1 && opcion > 4);
+ 
  System.out.println("Ingrese el primer numero");
  a = teclado.nextInt();
+ 
  System.out.println("Ingrese el segundo numero");
  b = teclado.nextInt();
- if(opcion.equals(1)){
- resultado = a + b;
- }else if(opcion.equals(2)){
- resultado = a - b;
- }else if(opcion.equals(3)){
- resultado = a * b;
- }else{
- resultado = a / b;
- }
+ 
+ switch (opcion) {
+	case 1:
+		System.out.println("Elegiste la opcion suma");
+		resultado = a + b;
+		break;
+	case 2:
+		System.out.println("Elegiste la opcion resta");
+		resultado = a - b;
+		break;
+
+	case 3:
+		System.out.println("Elegiste la opcion multiplicacion");
+		resultado = a * b;
+		break;
+	case 4:
+		System.out.println("Elegiste la opcion division");
+		resultado = a / b;
+		break;
+	}
  System.out.println("El resultado es " + resultado.toString());
  }
 }
